6e0e974f3b5081be6b1dff77f7dde07178835534
Tomii0711 <73540968+Tomii0711@users.noreply.github.com>
2021-10-15 11:48:22 +0200

    Add files via upload

---
+++ Prog/CleanEating/Rostok/Rostok/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+//1. feladat
+namespace Rostok
+{
+    class Adat
+    {
+        public string Nev;
+        public string Kategoria;
+        public string Mennyiseg;
+        public double Tartalom;
+
+        public Adat(string [] sor)
+        {
+            Nev = sor[0];
+            Kategoria = sor[1];
+            Mennyiseg = sor[2];
+            Tartalom = Convert.ToDouble(sor[3]);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //segédváltozók
+            List<Adat> adatok = new List<Adat>();
+            int nemSzazas = 0, tartalomDB = 0;
+            double tartalomSum = 0;
+            HashSet<string> kategoriak = new HashSet<string>();
+            StreamWriter ki = new StreamWriter("Rostok100g.txt");
+            ki.WriteLine("Megnevezés;Kategória;Rost"); //fejléc
+
+            //2. feladat
+            StreamReader be = new StreamReader("rostok.txt");
+            be.ReadLine(); //fejléc
+            while (!be.EndOfStream)
+            {
+                Adat akt = new Adat(be.ReadLine().Split(';'));
+                adatok.Add(akt);
+
+                //4. feladathoz
+                if (akt.Mennyiseg != "100g")
+                {
+                    nemSzazas++;
+                }
+                //5. feladathoz
+                else
+                {
+                    if (akt.Kategoria == "Friss gyümölcsök")
+                    {
+                        tartalomSum += akt.Tartalom;
+                        tartalomDB++;
+                    }
+                    //9. feladathoz
+                    ki.WriteLine($"{akt.Nev};{akt.Kategoria};{akt.Tartalom}");
+                }
+
+                //7. feladathoz
+                kategoriak.Add(akt.Kategoria);
+            }
+            be.Close();
+            ki.Close();
+
+            //3. feladat
+            Console.WriteLine($"3. feladat: Élelmiszerek száma: {adatok.Count}");
+
+            //4. feladat
+            Console.WriteLine($"4. feladat: Nem 100g-os egység: {nemSzazas}");
+
+            //5. feladat
+            Console.WriteLine($"5. feladat: Friss gyümölcsök átlagos rosttartalma: {Math.Round(tartalomSum / tartalomDB, 14)}g");
+
+            //6. feladat
+            Console.Write("6. feladat: Kérek egy karakterláncot: ");
+            string beKar = Console.ReadLine().ToLower();
+            List<string> vannak = new List<string>();
+            for (int i = 0; i < adatok.Count; i++)
+            {
+                if (adatok[i].Nev.ToLower().Contains(beKar))
+                {
+                    vannak.Add("\t" + adatok[i].Nev + " @ " + adatok[i].Kategoria + " @ " + adatok[i].Mennyiseg + " @ " + adatok[i].Tartalom);
+                }
+            }
+            if (vannak.Count != 0)
+            {
+                for (int i = 0; i < vannak.Count; i++)
+                {
+                    Console.WriteLine(vannak[i]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("\tA keresés eredménytelen!");
+            }
+
+            //7. feladat
+            Console.WriteLine($"7. feladat: Kategóriák száma: {kategoriak.Count}");
+
+            //8. feladat
+            Console.WriteLine("8. feladat: Statisztika");
+            int[] katDB = new int[kategoriak.Count];
+            for (int i = 0; i < adatok.Count; i++)
+            {
+                int j = 0;
+                foreach (var item in kategoriak)
+                {
+                    if (adatok[i].Kategoria == item)
+                    {
+                        katDB[j]++;
+                        break;
+                    }
+                    j++;
+                }
+            }
+            int katInd = 0;
+            foreach (var item in kategoriak)
+            {
+                Console.WriteLine($"\t{item} - {katDB[katInd]}");
+                katInd++;
+            }
+
+            //9. feladat
+            Console.WriteLine("9. feladat: Rostok100g.txt");
+        }
+    }
+}
---
+++ Prog/balkezesek/balkezesek/balkezesek/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Globalization;
+
+//1. feladat
+namespace balkezesek
+{
+    class Adat
+    {
+        public string nev;
+        public DateTime elso;
+        public DateTime utolso;
+        public int suly;
+        public int magassag;
+
+        public double magassagCenti;
+
+        public Adat(string[] sor)
+        {
+            nev = sor[0];
+            string[] seged = sor[1].Split('-');
+            elso = new DateTime(Convert.ToInt32(seged[0]), Convert.ToInt32(seged[1]), Convert.ToInt32(seged[2]));
+            seged = sor[2].Split('-');
+            utolso = new DateTime(Convert.ToInt32(seged[0]), Convert.ToInt32(seged[1]), Convert.ToInt32(seged[2]));
+            suly = Convert.ToInt32(sor[3]);
+            magassag = Convert.ToInt32(sor[4]);
+            magassagCenti = magassag * 2.54;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //segéd változók
+            List<Adat> adatok = new List<Adat>();
+
+            //2. feladat
+            StreamReader be = new StreamReader("balkezesek.csv");
+            be.ReadLine();
+            while (!be.EndOfStream)
+            {
+                Adat akt = new Adat(be.ReadLine().Split(';'));
+                adatok.Add(akt);
+            }
+            be.Close();
+
+            //3. feladat
+            Console.WriteLine($"3. feladat: {adatok.Count}");
+
+            //4. feladat
+            Console.WriteLine("4. feladat:");
+            for (int i = 0; i < adatok.Count; i++)
+            {
+                if (adatok[i].utolso.Year == 1999 && adatok[i].utolso.Month == 10)
+                {
+                    Console.WriteLine($"\t{adatok[i].nev}, {adatok[i].magassagCenti.ToString("0.0")} cm");
+                }
+            }
+
+            //5. feladat
+            Console.WriteLine("5. feladat:");
+            int beEv = 0;
+            do
+            {
+                Console.Write("Kérek egy 1990 és 1999 közötti évszámot!: ");
+                beEv = Convert.ToInt32(Console.ReadLine());
+                if (beEv < 1990 || beEv > 1999)
+                {
+                    Console.Write("Hibás adat!");
+                }
+                else
+                {
+                    break;
+                }
+            } while (true);
+            Console.WriteLine(beEv);
+
+            //6. feladat
+            double sulySzumm = 0;
+            double sulyDB = 0;
+            for (int i = 0; i < adatok.Count; i++)
+            {
+                if (adatok[i].elso.Year <= beEv && adatok[i].utolso.Year >= beEv)
+                {
+                    sulySzumm += adatok[i].suly;
+                    sulyDB++;
+                }
+            }
+            Console.WriteLine($"6. feladat: {Math.Round(sulySzumm/sulyDB, 2)} font");
+        }
+    }
+}
